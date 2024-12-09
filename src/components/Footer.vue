d85665fb4308801a0454c528559d8c742c7db2f6
Евгений Осипов <C:\Users\osipov.em\AppData\Roaming\The Bat!>
2024-12-09 16:09:40 +0300

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,82 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="footer-box">
+        <router-link to="/" class="footer-logo logo">
+          <img src="../assets/images/logo.png" alt="logo">
+        </router-link>
+        <div class="footer-nav">
+          <ul>
+            <li><router-link to="/rest">Ресторанам</router-link></li>
+            <li><router-link to="/rest">Курьерам</router-link></li>
+            <li><router-link to="/rest">Пресс-центр</router-link></li>
+            <li><router-link to="/rest">Контакты</router-link></li>
+          </ul>
+        </div>
+        <div class="footer-social">
+          <img src="../assets/images/social/inst.png" alt="inst">
+          <img src="../assets/images/social/fb.png" alt="fb">
+          <img src="../assets/images/social/vk.png" alt="vk">
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.footer {
+  min-height: 156px;
+  display: flex;
+  align-items: center;
+  padding: 60px 0;
+}
+
+.footer-box {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 36px;
+  flex-wrap: wrap;
+}
+
+.footer-nav {
+  flex-grow: 1;
+}
+
+.footer-nav ul {
+  display: flex;
+  align-items: center;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  gap: 15px;
+  flex-wrap: wrap;
+}
+
+.footer-nav ul li {
+  font-weight: 400;
+  font-size: 18px;
+  color: #595959;
+}
+
+.footer-nav ul li a {
+  text-decoration: none;
+  font-weight: 400;
+  font-size: 18px;
+  color: #595959;
+}
+
+.footer-social {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 20px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,68 @@
+<template>
+    <section class="banner">
+        <div class="container">
+            <div class="banner-item">
+                <h2 class="banner-item--title">
+                    Онлайн-сервис доставки еды на дом
+                </h2>
+                <p class="banner-item--subtitle">
+                    Блюда из любимого ресторана привезет курьер в перчатках, маске и c антисептиком
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <section class="products">
+        <div class="container">
+            <div class="products-header">
+                <h3 class="products-header--title">
+                    Рестораны
+                </h3>
+                <input type="text" placeholder="Поиск блюд и ресторанов" class="products-header--search" name="search">
+            </div>
+
+            <div class="products-wrapper" id="rest-container">
+
+                <router-link :to="`/rest?id=${rest.id}`" class="products-card" v-for="rest in store.rests" :key="rest.id">
+                    <div class="products-card__image">
+                        <img :src="getUrl(rest.image)" alt="rest-1">
+                    </div>
+                    <div class="products-card__description">
+                        <div class="products-card__description-row">
+                            <h4 class="products-card__description--title">
+                                {{ rest.title }}
+                            </h4>
+                            <div class="products-card__description--badge">
+                                {{ rest.time }} мин
+                            </div>
+                        </div>
+                        <div class="products-card__description-row">
+                            <div class="products-card__description-info">
+                                <div class="products-card__description-info--raiting">
+                                    <img src="../assets/images/icons/star.svg" alt="star">
+                                    {{ rest.raiting }}
+                                </div>
+                                <div class="products-card__description-info--price">
+                                    От {{ rest.price }} ₽
+                                </div>
+                                <div class="products-card__description-info--group">
+                                    {{ rest.type }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </router-link>
+
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup>
+import { store } from '../store/index'
+
+const getUrl = (name) => {
+    return new URL(`../assets/images/rests/${name}`, import.meta.url)
+    // return `../assets/images/rests/rest-1.jpg`
+}
+</script>
